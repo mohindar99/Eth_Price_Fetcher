da1287eb645c41883b84b90e7bb02953da152e0b
mohindar99 <d.mohindar1999@gmail.com>
2023-01-13 22:46:39 +0530

    First commit

---
+++ contracts/converter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+    }
+
+    //sending value of eth
+    function getETHPrice(int256 val) public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price * val;
+    }
+}
